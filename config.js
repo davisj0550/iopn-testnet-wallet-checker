896d9c797fbf1ebf51f5e57e9acd594ca530c9a9
davisj0550 <davisj0550@gmail.com>
2026-03-09 16:34:55 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const RPC_URL="https://testnet-rpc.iopn.tech"
+
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+
+const TOKENS=[
+{
+name:"Test USDT",
+symbol:"USDT",
+address:"0x0000000000000000000000000000000000000000"
+}
+]
